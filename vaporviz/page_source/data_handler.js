1caad81330350b29c62424285ef52ce4c93e1555
Glen Coppersmith <coppersmith@jhu.edu>
2014-04-29 10:16:59 -0400

    Added functionality for the rest of the database interface, abstracted out some of the generic pieces of the db call

--- vaporviz/page_source/data_handler.js
+++ vaporviz/page_source/data_handler.js
@@ -10,6 +10,19 @@ function display_annotations(annotations){
     $('#annotations_landing_zone').html(prettyPrint(annotations));
 };
 
+function display_utterances(utterances){
+    $('#utterances_landing_zone').html(prettyPrint(utterances));
+};
+
+function display_pseudoterms(pseudoterms){
+    $('#pseudoterms_landing_zone').html(prettyPrint(pseudoterms));
+};
+
+function display_audio_events(audio_events){
+    $('#audio_events_landing_zone').html(prettyPrint(audio_events));
+};
+
+
 function get_annotation(){
     //dataset_name = $('#dataset_dropdown').val();
 
@@ -39,10 +52,95 @@ function get_annotation(){
 }
 
 
+function get_utterances(){
+    //dataset_name = $('#dataset_dropdown').val();
+
+    send = {};
+    send.dataset='buckeye' // HARDCODE
+    send.count = 10; //TODO: pull from some element
+    console.log(send)
+
+    $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+                dataType: "json"
+                });
+
+    $.ajax({
+            url: "http://localhost:12321/find_utterances",
+                type: "POST",
+                data: JSON.stringify(send),
+                error: function(xhr, error) {
+                alert('Error!  Status = ' + xhr.status + ' Message = ' + error);
+            },
+                success: function(data) {
+                current_utterances = data;
+                display_utterances(data);
+            } 
+        });
+}
+
+
+function get_audio_events(){
+
+    send = {};
+    send.dataset='buckeye' // HARDCODE
+    send.count = 10; //TODO: pull from some element
+    console.log(send)
+
+    $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+                dataType: "json"
+                });
+
+    $.ajax({
+            url: "http://localhost:12321/find_audio_events",
+                type: "POST",
+                data: JSON.stringify(send),
+                error: function(xhr, error) {
+                alert('Error!  Status = ' + xhr.status + ' Message = ' + error);
+            },
+                success: function(data) {
+                current_audio_events = data;
+                display_audio_events(data);
+            } 
+        });
+}
+
+
+function get_pseudoterms(){
+
+    send = {};
+    send.dataset='buckeye' // HARDCODE
+    send.count = 10; //TODO: pull from some element
+    console.log(send)
+
+    $.ajaxSetup({
+            contentType: "application/json; charset=utf-8",
+                dataType: "json"
+                });
+
+    $.ajax({
+            url: "http://localhost:12321/find_pseudoterms",
+                type: "POST",
+                data: JSON.stringify(send),
+                error: function(xhr, error) {
+                alert('Error!  Status = ' + xhr.status + ' Message = ' + error);
+            },
+                success: function(data) {
+                current_pseudoterms = data;
+                display_pseudoterms(data);
+            } 
+        });
+}
+
+
 
 
 function test_ajax_calls(){
     get_annotation();
+    get_utterances();
+    get_audio_events();
+    get_pseudoterms();
 };
 
 
--- vaporviz/vapor_bottle.py
+++ vaporviz/vapor_bottle.py
@@ -9,7 +9,8 @@ except:
     import json
 
 from lib.database import init_dbconn
-from vaporgasp.queries import find_annotations
+from vaporgasp.queries import (find_annotations, find_utterances,
+                               find_pseudoterms, find_audio_events)
     
 # the decorator to ease some javascript pain (if memory serves)
 def enable_cors(fn):
@@ -47,20 +48,16 @@ def widget_handler(path):
     print "Returning what was found at:", source_path
     return page
 
-"""TODO: find_annotations is untested"""
-@route('/find_annotations',method=['OPTIONS','POST'])
-@json_wrapper
-@enable_cors
-def find_annotation_handler():
-    metadata_filters = json.load(request.body)
+
+def generic_find(find_function, metadata_filters):
+    """Does all the requisite conversions for pulling from the database
+    and preparing to serve to the website.
+    `find_function` should be one of the function from queries.py"""
     print metadata_filters
     dataset = metadata_filters['dataset'] #This maps to MongoDB table name
     print "from dataset:", dataset
     del metadata_filters['dataset']
 
-
-
-    
     #SECURITY add whitelist of dataset names permitted
     db = init_dbconn(name = dataset, host='r4n7')
 
@@ -78,7 +75,7 @@ def find_annotation_handler():
         sort_by = metadata_filters['sort_by'] #Or do we need a conversion here
 
     print metadata_filters
-    cursor = find_annotations(db, **metadata_filters)
+    cursor = find_function(db, **metadata_filters)
 
     #Accumulate results
     results = []
@@ -87,6 +84,49 @@ def find_annotation_handler():
         results.append(result)
     print "Results:", results
     return results
+    
+
+
+@route('/find_annotations',method=['OPTIONS','POST'])
+@json_wrapper
+@enable_cors
+def find_annotations_handler():
+    metadata_filters = json.load(request.body)
+    print metadata_filters
+    results = generic_find( find_annotations, metadata_filters)
+    print "Results:", results
+    return results
+
+@route('/find_utterances',method=['OPTIONS','POST'])
+@json_wrapper
+@enable_cors
+def find_utterances_handler():
+    metadata_filters = json.load(request.body)
+    print metadata_filters
+    results = generic_find( find_utterances, metadata_filters)
+    print "Results:", results
+    return results
+
+@route('/find_audio_events',method=['OPTIONS','POST'])
+@json_wrapper
+@enable_cors
+def find_audio_events_handler():
+    metadata_filters = json.load(request.body)
+    print metadata_filters
+    results = generic_find( find_audio_events, metadata_filters)
+    print "Results:", results
+    return results
+
+@route('/find_pseudoterms',method=['OPTIONS','POST'])
+@json_wrapper
+@enable_cors
+def find_pseudoterms_handler():
+    metadata_filters = json.load(request.body)
+    print metadata_filters
+    results = generic_find( find_pseudoterms, metadata_filters)
+    print "Results:", results
+    return results
+
 
 
 parser = argparse.ArgumentParser()
