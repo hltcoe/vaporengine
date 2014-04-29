176932d273002dc6b73efd8f3b37b4478f4ca130
Craig Harman <craig@craigharman.net>
2014-04-29 17:23:46 -0400

    Updated pysox_test.py to use temporary files

--- pysox_test.py
+++ pysox_test.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import os
+import tempfile
+
 import pysox
 
 INPUT_FILES = ['0003.wav', '0004.wav']
 OUTPUT_FILE = 'out.wav'
 
+# Create a temporary directory
+tmp_directory = tempfile.mkdtemp()
+tmp_filename = os.path.join(tmp_directory, 'combined_clips.wav')
+
+# The first argument to CSoxStream must be a filename with a '.wav' extension.
+#
+# If the output file does not have a '.wav' extension, pysox will raise
+# an "IOError: No such file" exception.
 outfile = pysox.CSoxStream(
-    OUTPUT_FILE,
+    tmp_filename,
     'w',
     pysox.CSignalInfo(8000.0,1,14))  # TODO: Don't hard-code sample rate
 
@@ -21,3 +32,11 @@ for input_filename in INPUT_FILES:
     infile.close()
 
 outfile.close()
+
+# Read in audio data from temporary file
+wav_data = open(tmp_filename, 'rb').read()
+open(OUTPUT_FILE, 'wb').write(wav_data)
+
+# Clean up temporary files
+os.remove(tmp_filename)
+os.rmdir(tmp_directory)
