e2e4a25b76d9a1eeb227a817c78747040160df74
Craig Harman <craig@craigharman.net>
2014-05-01 06:04:07 -0400

    Updated signal parameters for dynamically created 'buckeye' snippets

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ buckeye = {}
 buckeye['DB_HOST'] = 'r4n7'
 buckeye['DB_NAME'] = 'buckeye'
 buckeye['DB_PORT'] = 27017
-buckeye['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(8000.0,1,14)
+buckeye['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
