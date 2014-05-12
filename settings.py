5d85bc75283a8f9198e3b5a959fd468065f69ea8
Craig Harman <craig@craigharman.net>
2014-05-12 12:35:40 -0400

    Path to ZRL files now in settings.py.  Added 'buckeye_localhost' config.

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+import os
 import pysox
 
 buckeye = {}
@@ -5,3 +6,11 @@ buckeye['DB_HOST'] = 'r4n7'
 buckeye['DB_NAME'] = 'buckeye'
 buckeye['DB_PORT'] = 27017
 buckeye['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
+buckeye['ZRL_PATH'] = '/home/hltcoe/ajansen/discovery/exp/buckeye-T25/'
+
+buckeye_localhost = {}
+buckeye_localhost['DB_HOST'] = 'localhost'
+buckeye_localhost['DB_NAME'] = 'buckeye'
+buckeye_localhost['DB_PORT'] = 27017
+buckeye_localhost['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
+buckeye['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
