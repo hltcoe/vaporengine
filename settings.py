f84bcb7082622bbb3a439b71b22e51cde1e21279
Craig Harman <craig@craigharman.net>
2014-05-12 15:21:55 -0400

    Added WAV_PATH variable to settings.py

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ buckeye['DB_HOST'] = 'r4n7'
 buckeye['DB_NAME'] = 'buckeye'
 buckeye['DB_PORT'] = 27017
 buckeye['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
+buckeye['WAV_PATH'] = '/home/hltcoe/ajansen/aren_local/BUCKEYE'
 buckeye['ZRL_PATH'] = '/home/hltcoe/ajansen/discovery/exp/buckeye-T25/'
 
 buckeye_localhost = {}
@@ -13,4 +14,5 @@ buckeye_localhost['DB_HOST'] = 'localhost'
 buckeye_localhost['DB_NAME'] = 'buckeye'
 buckeye_localhost['DB_PORT'] = 27017
 buckeye_localhost['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
+buckeye['WAV_PATH'] = os.path.join(os.getenv('HOME'), 'BUCKEYE')
 buckeye_localhost['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
