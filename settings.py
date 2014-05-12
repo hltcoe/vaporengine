c01a139f235cd3802fed32058f1e28dfaaf6f989
Craig Harman <craig@craigharman.net>
2014-05-12 13:00:35 -0400

    Added missing setting for 'buckeye_localhost' config

--- settings.py
+++ settings.py
@@ -13,4 +13,4 @@ buckeye_localhost['DB_HOST'] = 'localhost'
 buckeye_localhost['DB_NAME'] = 'buckeye'
 buckeye_localhost['DB_PORT'] = 27017
 buckeye_localhost['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
-buckeye['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
+buckeye_localhost['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
