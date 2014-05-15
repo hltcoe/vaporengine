2141a85b38bb29fd0311704b02215ab3b81d25c8
Craig Harman <craig@craigharman.net>
2014-05-15 10:50:10 -0400

    Fixed error in 'buckeye_localhost' config in settings.py

--- settings.py
+++ settings.py
@@ -14,5 +14,5 @@ buckeye_localhost['DB_HOST'] = 'localhost'
 buckeye_localhost['DB_NAME'] = 'buckeye'
 buckeye_localhost['DB_PORT'] = 27017
 buckeye_localhost['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(16000.0,1,16)
-buckeye['WAV_PATH'] = os.path.join(os.getenv('HOME'), 'BUCKEYE')
+buckeye_localhost['WAV_PATH'] = os.path.join(os.getenv('HOME'), 'BUCKEYE')
 buckeye_localhost['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
