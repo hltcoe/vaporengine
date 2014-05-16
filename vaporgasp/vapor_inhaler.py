73bb117a7a9d3dd9912e802b3231235b7197b968
Craig Harman <craig@craigharman.net>
2014-05-16 12:10:14 -0400

    Fixed bug where audio events were being assigned wrong utterance.

--- vaporgasp/vapor_inhaler.py
+++ vaporgasp/vapor_inhaler.py
@@ -18,45 +18,46 @@ db = init_dbconn(host=dbhost,name=dbname)
 zrl = ZRLoader(settings['ZRL_PATH'], clusters=settings['ZRL_CLUSTERS'])
 
 
-ae_to_mongo_id = {}
-pt_to_mongo_id = {}
+audioevent_to_mongo_id = {}
+pseudoterm_to_mongo_id = {}
+utterance_to_mongo_id = {}
 
 for utterance in zrl.AllUtterances():
     utterance_fields = {
         'hltcoe_audio_path': zrl.filename_for_utterance[utterance]
         }
     utterance_mongo_id = insert_utterance(db, utterance_fields)
+    utterance_to_mongo_id[utterance] = utterance_mongo_id
 
     this_utterance_pts = []
-    
-    for ptid in zrl.PTIDsForUtterance(utterance):
+    for pseudoterm_id in zrl.PTIDsForUtterance(utterance):
         # If we haven't seen this pseudoterm before, insert it
-        if not ptid in pt_to_mongo_id:
-            pt = {'eng_display': 'pt%s'%ptid,
-                  'native_display': 'PT%s'%ptid,
-                  'zr_pt_id':ptid}
-            mongo_ptid = insert_pseudoterm(db,pt)
-            pt_to_mongo_id[ptid] = mongo_ptid
-        mongo_ptid = pt_to_mongo_id[ptid]
-        this_utterance_pts.append(mongo_ptid)
-            
-        for aeid in zrl.AudioEventIDsForPTID(ptid):
-            if not aeid in ae_to_mongo_id:
-                filename,start,end = zrl.AudioEventDataForAEID(aeid)
-
-                ae = { 'start_offset':start,
-                       'end_offset':end,
-                       'duration':end-start,
-                       'zr_pt_id':ptid,
-                       'pt_id': pt_to_mongo_id[ptid],
-                       'utterance_id': utterance_mongo_id}
-
-                mongo_aeid = insert_audio_event(db, ae)
-                ae_to_mongo_id[aeid] = mongo_aeid
-            
-            #print "%s %s %s %s" % (utterance, ptid, aeid, zrl.AudioEventDataForAEID(aeid))
+        if not pseudoterm_id in pseudoterm_to_mongo_id:
+            pt = {'eng_display': 'pt%s'%pseudoterm_id,
+                  'native_display': 'PT%s'%pseudoterm_id,
+                  'zr_pt_id':pseudoterm_id}
+            mongo_pseudoterm_id = insert_pseudoterm(db,pt)
+            pseudoterm_to_mongo_id[pseudoterm_id] = mongo_pseudoterm_id
+        mongo_pseudoterm_id = pseudoterm_to_mongo_id[pseudoterm_id]
+        this_utterance_pts.append(mongo_pseudoterm_id)
 
     utterance_fields['pts'] = this_utterance_pts
-    print "Pseudoterms for utterance",utterance_mongo_id,":", utterance_fields['pts']
+    print "Pseudoterms for utterance", utterance_mongo_id, ":", utterance_fields['pts']
     print utterance_fields.keys()
-    update_utterance( db, utterance_mongo_id, **utterance_fields)
+    update_utterance(db, utterance_mongo_id, **utterance_fields)
+
+for utterance in zrl.AllUtterances():
+    for pseudoterm_id in zrl.PTIDsForUtterance(utterance):
+        for audioevent_id in zrl.AudioEventIDsForPTID(pseudoterm_id):
+            if not audioevent_id in audioevent_to_mongo_id:
+                utterance,start,end = zrl.AudioEventDataForAEID(audioevent_id)
+
+                ae = { 'start_offset': start,
+                       'end_offset': end,
+                       'duration': end-start,
+                       'zr_pt_id': pseudoterm_id,
+                       'pt_id': pseudoterm_to_mongo_id[pseudoterm_id],
+                       'utterance_id': utterance_to_mongo_id[utterance]}
+
+                mongo_audioevent_id = insert_audio_event(db, ae)
+                audioevent_to_mongo_id[audioevent_id] = mongo_audioevent_id
