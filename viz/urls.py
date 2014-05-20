50a835d4c4b7c7eafe061a89129eeff6cf5a9bc6
Craig Harman <craig@craigharman.net>
2014-05-20 17:35:27 -0400

    Added stub for corpus_view view.

--- viz/urls.py
+++ viz/urls.py
@@ -5,6 +5,8 @@ admin.autodiscover()
 
 from viz import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.home, name='home'),
+    url(r'^corpus/(?P<corpus_name>\w+)/$', views.corpus_overview, name='corpus_overview'),
 )
--- viz/views.py
+++ viz/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def corpus_overview(request, corpus_name):
+    # Add list of Documents/Utterances to corpus_overview
+    return HttpResponse(corpus_name)
+
 
 def home(request):
+    # TODO: Add list of current corpora to homepage
     return render(request, "home.html")
