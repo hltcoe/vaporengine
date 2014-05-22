7edfc4e218b8903be5c784555a18f905bf76d854
Craig Harman <craig@craigharman.net>
2014-05-22 13:09:08 -0400

    Fixed spelling of tagalog.

--- settings.py
+++ settings.py
@@ -24,6 +24,7 @@ buckeye_localhost['ZRL_CLUSTERS'] = 'matches/master_graph.dedups'
 buckeye_localhost['ZRL_PATH'] = os.path.join(os.getenv('HOME'), 'buckeye-T25/')
 settings['buckeye_localhost'] = buckeye_localhost
 
+
 fisher_spanish = {}
 fisher_spanish['DB_HOST'] = 'r4n7'
 fisher_spanish['DB_NAME'] = 'fisher_spanish'
@@ -35,14 +36,14 @@ fisher_spanish['ZRL_PATH'] = '/home/hltcoe/ajansen/discovery/exp/fishsp200/'
 settings['fisher_spanish'] = fisher_spanish
 
 
-tagolog = {}
-tagolog['DB_HOST'] = 'r4n7'
-tagolog['DB_NAME'] = 'tagolog'
-tagolog['DB_PORT'] = 27017
-tagolog['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(8000.0,1,16)
-tagolog['WAV_PATH'] = '/home/hltcoe/ajansen/discovery/exp/tagolog300'
-tagolog['ZRL_CLUSTERS'] = 'matches/master_graph.dedups'
-tagolog['ZRL_PATH'] = '/home/hltcoe/ajansen/discovery/exp/tagolog300/'
-settings['tagolog'] = tagolog
+tagalog = {}
+tagalog['DB_HOST'] = 'r4n7'
+tagalog['DB_NAME'] = 'tagalog'
+tagalog['DB_PORT'] = 27017
+tagalog['SOX_SIGNAL_INFO'] = pysox.CSignalInfo(8000.0,1,16)
+tagalog['WAV_PATH'] = '/home/hltcoe/ajansen/discovery/exp/tagalog300'
+tagalog['ZRL_CLUSTERS'] = 'matches/master_graph.dedups'
+tagalog['ZRL_PATH'] = '/home/hltcoe/ajansen/discovery/exp/tagalog300/'
+settings['tagalog'] = tagalog
 
-current_corpora = ['buckeye', 'fisher_spanish', 'tagolog']
+current_corpora = ['buckeye', 'fisher_spanish', 'tagalog']
