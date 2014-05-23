13063cbae723500f09799e4935f039c01a52d71d
Craig Harman <craig@craigharman.net>
2014-05-23 11:01:21 -0400

    Minimized number of Mongo queries made by make_wc_datastructure().
    Previously, make_wc_datastructure() made:
    
      total_utterances + total_psuedoterms_for_these_utterances
    
    MongoDB queries.  Now, make_wc_datastructure() makes only two MongoDB
    queries - one to retrieve the list of utterances, and the second to
    retrieve the pseudoterms for those utterances.

--- vaporviz/vapor_venncloud.py
+++ vaporviz/vapor_venncloud.py
@@ -1,5 +1,3 @@
-from __future__ import division
-
 """Specific code for connecting dynamic wordclouds and vennclouds to vaporviz"""
 
 """vennclouds need data of the form:
@@ -12,67 +10,58 @@ from __future__ import division
 token dict: { token: 'display_token', tf:tf, idf:idf, examples:[examples], ... }
 """
 
-#from vaporgasp.queries import (find_pseudoterms_from_eng_label, find_pseudoterms,
-from vaporgasp.queries import (find_pseudoterms,
-                               find_utterances)
+from __future__ import division
 
 from bson import ObjectId
-def make_wc_datastructure(db, utterances ):
-    """Take a list of utterances and return the relevant
-    token vectors, as needed by the dynamic wordcloud/venncloud code. """
 
-    docs = []
+from vaporgasp.queries import find_pseudoterms, find_utterances
 
-    token_to_mongoids = {} #for graphemes that line up, these will be lists
-    mongoid_to_token = {} #So we don't have to pull the same PT multiple times
 
-    #loop through utterances pulling out pseudoterms that occur in them
+
+def make_wc_datastructure(db, utterances):
+    """
+    Take a list of utterances and return the relevant token vectors,
+    as needed by the dynamic wordcloud/venncloud code.
+    """
+    pseudoterm_id_set = set()
+    tokens = []
+    token_to_pseudoterm_ids = {} #for graphemes that line up, these will be lists
+    utterance_object_ids = []
+
     for raw_utt_id in utterances:
-        if type(raw_utt_id) == type(" "):
+        if type(raw_utt_id) in [str, unicode]:
             utt_id = ObjectId(raw_utt_id)
-        else:
+        elif type(raw_utt_id) == ObjectId:
             utt_id = raw_utt_id
-        utt = find_utterances(db, _id=utt_id ).next()
-        this_doc = []
-        #loop through pseudoterms and make a document out of them
-        for pt_id in utt['pts']:
+        else:
+            raise TypeError("raw_utt_id type was: '%s'" % type(raw_utt_id))
+        utterance_object_ids.append(utt_id)
 
-            junk_pt = False
-            #If we've seen it before, don't hit the DB, just pull out the token
-            if pt_id in mongoid_to_token:
-                token = mongoid_to_token[pt_id]
-            else:
-                #If we haven't seen it, hit the DB, add to appropriate places
-                pt_cur = find_pseudoterms(db, _id=ObjectId(pt_id))
-                try:
-                    pt = pt_cur.next()
-                    token = pt['eng_display']
-                except StopIteration:
-                    junk_pt = True
-                else:
-                    token_to_mongoids[token] = token_to_mongoids.get(token,[]) + [pt_id]
-                    mongoid_to_token[pt_id] = token
-            if not junk_pt:
-                this_doc.append( token )
-        docs.append(sorted(this_doc))
+    utterances_cursor = db.utterances.find({"_id": {"$in": utterance_object_ids}})
+    for utterance in utterances_cursor:
+        for pseudoterm_id in utterance['pts']:
+            pseudoterm_id_set.add(pseudoterm_id)
+    pseudoterm_object_ids = list(pseudoterm_id_set)
+
+    pseudoterms_cursor = db.pseudoterms.find({"_id": {"$in": pseudoterm_object_ids}})
+    for pseudoterm in pseudoterms_cursor:
+        token = pseudoterm['eng_display']
+        tokens.append(token)
+        token_to_pseudoterm_ids[token] = token_to_pseudoterm_ids.get(token,[]) + [pseudoterm['_id']]
 
     #Have to get idf somehow to include. For now everything is 1
 
     #Figure out the right way to integrate dynamic wordcloud package internal calls
     tf = {}
-    for doc in docs:
-        for token in doc:
-            tf[token] = tf.get(token,0)+1
+    for token in tokens:
+        tf[token] = tf.get(token,0)+1
 
     #Now finally make the token feature vector
     token_vector = []
-    for token,mongo_ids in sorted(token_to_mongoids.items()):
+    for token, pseudoterm_ids in sorted(token_to_pseudoterm_ids.items()):
         t = {'text':token, 'tf':tf[token], 'idf':1, 'examples':[],
-             'number_of_pts':len(mongo_ids), 'pt_ids':mongo_ids} #TODO fix IDF
+             'number_of_pts':len(pseudoterm_ids), 'pt_ids':pseudoterm_ids} #TODO fix IDF
         #TODO add pseudoterm length here too?
         token_vector.append(t)
-        
-    return token_vector
-    
-    
 
+    return token_vector
