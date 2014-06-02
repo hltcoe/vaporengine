3caf81062a4c36e61ead8e8a45c510cbec54fdb5
Craig Harman <craig@craigharman.net>
2014-06-02 13:35:58 -0400

    Updated backup/restore scripts to use 'zr_pt_id' field as unique identifier for Pseudoterm, for better compatibility with annotation files that Aren generated for the Gujarati data.

--- vaporgasp/backup_annotations.py
+++ vaporgasp/backup_annotations.py
@@ -12,12 +12,12 @@ parser.add_argument("dataset_name")
 parser.add_argument("backup_file")
 args = parser.parse_args()
 
-db = init_dbconn(name=settings[args.datset_name]['DB_NAME'], host=settings[args.dataset_name]['DB_HOST'])
+db = init_dbconn(name=settings[args.dataset_name]['DB_NAME'], host=settings[args.dataset_name]['DB_HOST'])
 
 backup_file = codecs.open(args.backup_file, "w", encoding="utf-8")
 
 for pseudoterm in find_pseudoterms(db, annotated=True):
     print pseudoterm
-    backup_file.write("%s\t%s\n" % (pseudoterm['native_display'], pseudoterm['eng_display']))
+    backup_file.write("%s\t%s\n" % (pseudoterm['zr_pt_id'], pseudoterm['eng_display']))
 
 backup_file.close()
--- vaporgasp/restore_annotations.py
+++ vaporgasp/restore_annotations.py
@@ -19,12 +19,13 @@ db = init_dbconn(name=settings[args.dataset_name]['DB_NAME'], host=settings[args
 backup_file = codecs.open(args.backup_file, "r", encoding="utf-8")
 
 for line in backup_file.readlines():
-    pseudoterm_name, annotation = line.strip().split('\t')
+    zr_pt_id, annotation = line.strip().split('\t')
+    zr_pt_id = int(zr_pt_id)
     try:
-        pseudoterm = find_pseudoterms(db, native_display=pseudoterm_name)[0]
-        update = {'annotated': True, 'display_name': annotation}
+        pseudoterm = find_pseudoterms(db, zr_pt_id=zr_pt_id)[0]
+        update = {'annotated': True, 'eng_display': annotation}
         update_pseudoterm(db, pseudoterm['_id'], **update)
     except IndexError:
-        print "Unable to find Pseudoterm with ID '%s'" % pseudoterm_name
+        print "Unable to find Pseudoterm with ID '%s'" % zr_pt_id
 
 backup_file.close()
