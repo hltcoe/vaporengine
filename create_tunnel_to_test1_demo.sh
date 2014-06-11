5cce923162295cb7663f77eea00d1fb235edbdfc
Craig Harman <craig@craigharman.net>
2014-06-11 16:48:22 -0400

    Added script to create ssh tunnel to VaporEngine demo on test1

---
+++ create_tunnel_to_test1_demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "ERROR: Please specify your COE username when invoking this script:"
+    echo
+    echo "  $0 [coe_username] "
+    echo
+    exit
+fi
+
+echo "Creating SSH tunnel to VaporEngine server running on test1..."
+echo
+echo "Point your browser at:"
+echo "  http://localhost:12321/"
+echo 
+echo "[Kill this script when you want to close the tunnel]"
+ssh $1@external.hltcoe.jhu.edu -L 12321:test1:12321 -N
