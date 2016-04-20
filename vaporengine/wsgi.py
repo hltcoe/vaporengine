c515581cc05c56d58b67a119ac4801f982ea19c6
Craig Harman <charman@debian>
2016-04-20 01:04:31 -0400

    wsgi.py now checks for a Python virtual environment

--- vaporengine/wsgi.py
+++ vaporengine/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Activate the virtual environment, if it exists.
+# This code was tested with Apache 2.2 on Debian 7
+VIRTUALENV_PATH=os.path.join(os.path.dirname(__file__), "../venv/")
+if os.path.isdir(VIRTUALENV_PATH):
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    activate_env=os.path.join(VIRTUALENV_PATH, "bin/activate_this.py")
+    execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 
