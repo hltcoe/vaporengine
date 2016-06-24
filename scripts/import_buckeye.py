bb51051ecbfb5b0f94874126a8b1502ce82e709f
Craig Harman <craig@craigharman.net>
2016-06-24 17:28:36 -0400

    Updated calls to corpus.create_from_zr_output() in import scripts
    The function parameters to corpus.create_from_zr_output() are now
    being passed in as named parameters, to make it clearer to (code)
    readers what the function is doing.

--- scripts/import_buckeye.py
+++ scripts/import_buckeye.py
@@ -6,9 +6,11 @@ def run():
     corpus_path = os.path.join(os.getenv('HOME'), 'zr_datasets/buckeye-T25/')
     corpus = Corpus()
     corpus.create_from_zr_output(
-        "Buckeye",
-        os.path.join(corpus_path, "matches/master_graph.nodes"),
-        os.path.join(corpus_path, "matches/master_graph.dedups"),
-        os.path.join(corpus_path, "fileswav.lst"),
-        16000, 1, 16)
+        corpus_name="Buckeye",
+        audiofragments=os.path.join(corpus_path, "matches/master_graph.nodes"),
+        clusters=os.path.join(corpus_path, "matches/master_graph.dedups"),
+        filenames=os.path.join(corpus_path, "fileswav.lst"),
+        audio_rate=16000,
+        audio_channels=1,
+        audio_precision=16)
     corpus.save()
--- scripts/import_daps.py
+++ scripts/import_daps.py
@@ -6,9 +6,11 @@ def run():
     corpus_path = os.path.join(os.getenv('HOME'), 'zr_datasets/daps/')
     corpus = Corpus()
     corpus.create_from_zr_output(
-        "DAPS",
-        os.path.join(corpus_path, "matches/master_graph.nodes"),
-        os.path.join(corpus_path, "matches/master_graph.dedups"),
-        os.path.join(corpus_path, "files.lst"),
-        44100, 1, 16)
+        corpus_name="DAPS",
+        audiofragments=os.path.join(corpus_path, "matches/master_graph.nodes"),
+        clusters=os.path.join(corpus_path, "matches/master_graph.dedups"),
+        filenames=os.path.join(corpus_path, "files.lst"),
+        audio_rate=44100,
+        audio_channels=1,
+        audio_precision=16)
     corpus.save()
