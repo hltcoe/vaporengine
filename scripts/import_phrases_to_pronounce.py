4e3c0b5b40be4cd3f4c302367aca20a686723a1c
Craig Harman <craig@craigharman.net>
2017-08-15 08:46:48 -0400

    Revised 'pronounce' index phase to only show phrase corpus title

--- scripts/import_phrases_to_pronounce.py
+++ scripts/import_phrases_to_pronounce.py
@@ -4,10 +4,20 @@ from pronounce.models import Phrase, PhraseCorpus
 
 def run():
     pc = PhraseCorpus()
-    pc.title = 'sample_phrases.txt'
+    pc.title = 'Oromo Phrases'
     pc.save()
 
-    for (line_index, line) in enumerate(codecs.open('sample_phrases.txt', encoding='utf-8')):
+    for (line_index, line) in enumerate(codecs.open('oromo_phrases.txt', encoding='utf-8')):
+        phrase = Phrase()
+        phrase.phrase_corpus = pc
+        phrase.phrase_index = line_index
+        phrase.create_from_line(line)
+
+    pc = PhraseCorpus()
+    pc.title = 'Tigrinya Phrases'
+    pc.save()
+
+    for (line_index, line) in enumerate(codecs.open('tigrinya_phrases.txt', encoding='utf-8')):
         phrase = Phrase()
         phrase.phrase_corpus = pc
         phrase.phrase_index = line_index
